caf1ccd994535c7f479bab0341223a7e69c81915
Fab <fabianjarrett@gmail.com>
2023-09-20 22:10:46 +0100

    Move deck logic to its own file

---
+++ deck_test.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	deck := NewDeck()
+	if len(deck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(deck))
+	}
+}
+
+func TestTakeRandomCard(t *testing.T) {
+	deck := NewDeck()
+	card, _ := deck.TakeRandomCard()
+	if len(deck) != 51 {
+		t.Errorf("Expected deck length of 51, but got %v", len(deck))
+	}
+	if card.Rank == "" {
+		t.Errorf("Expected card to have a rank, but got %v", card.Rank)
+	}
+	if card.Suit == "" {
+		t.Errorf("Expected card to have a suit, but got %v", card.Suit)
+	}
+}
+
+func TestTakeAllCards(t *testing.T) {
+	deck := NewDeck()
+	for i := 0; i < 52; i++ {
+		_, err := deck.TakeRandomCard()
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
+	}
+	if len(deck) != 0 {
+		t.Errorf("Expected deck length of 0, but got %v", len(deck))
+	}
+}
+
+func TestTakeRandomCardWithEmptyDeck(t *testing.T) {
+	deck := Deck{}
+	card, err := deck.TakeRandomCard()
+	if err == nil {
+		t.Errorf("Expected error, but got %v", card)
+	}
+	if len(deck) != 0 {
+		t.Errorf("Expected deck length of 0, but got %v", len(deck))
+	}
+}
+
+func DoCardsMatch(card1 Card, card2 Card) bool {
+	return card1.Rank == card2.Rank && card1.Suit == card2.Suit
+}
+
+func TestTakeRandomCardIsRandom(t *testing.T) {
+	deck := Deck{
+		Card{Rank: "Ace", Suit: "Hearts"},
+		Card{Rank: "Jack", Suit: "Diamonds"},
+		Card{Rank: "Queen", Suit: "Clubs"},
+	}
+	card1, _ := deck.TakeRandomCard()
+	card2, _ := deck.TakeRandomCard()
+	card3, _ := deck.TakeRandomCard()
+	if DoCardsMatch(card1, card2) || DoCardsMatch(card1, card3) || DoCardsMatch(card2, card3) {
+		t.Errorf("Expected cards to be different, but got %v, %v, %v", card1, card2, card3)
+	}
+}
+
+func TestCardValues(t *testing.T) {
+	testCases := []struct {
+		rank     string
+		expected int
+	}{
+		{"Ace", 1},
+		{"2", 2},
+		{"3", 3},
+		{"4", 4},
+		{"5", 5},
+		{"6", 6},
+		{"7", 7},
+		{"8", 8},
+		{"9", 9},
+		{"10", 10},
+		{"Jack", 10},
+		{"Queen", 10},
+		{"King", 10},
+	}
+	for _, tc := range testCases {
+		card := Card{Rank: tc.rank}
+		if card.GetValue() != tc.expected {
+			t.Errorf("%v should have a value of %v, but got %v", tc.rank, tc.expected, card.GetValue())
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,58 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand"
-	"strconv"
 )
-
-type Card struct {
-	Rank string
-	Suit string
-}
-
-func (c *Card) GetValue() int {
-	switch c.Rank {
-	case "Ace":
-		return 1
-	case "Jack", "Queen", "King":
-		return 10
-	default:
-		value, _ := strconv.Atoi(c.Rank)
-		return value
-	}
-}
-
-type Deck []Card
-
-func NewDeck() Deck {
-	ranks := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
-	suits := []string{"Hearts", "Diamonds", "Clubs", "Spades"}
-
-	deck := Deck{}
-
-	for _, suit := range suits {
-		for _, rank := range ranks {
-			card := Card{Rank: rank, Suit: suit}
-			deck = append(deck, card)
-		}
-	}
-
-	return deck
-}
-
-func (d *Deck) TakeRandomCard() (Card, error) {
-	if len(*d) == 0 {
-		return Card{}, errors.New("deck is empty")
-	}
-
-	randIndex := rand.Intn(len(*d))
-	card := (*d)[randIndex]
-	(*d)[randIndex] = (*d)[len(*d)-1]
-	*d = (*d)[:len(*d)-1]
-	return card, nil
-}
 
 func hit(card Card) {
 	fmt.Println("... Hit!")
